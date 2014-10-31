ee17365aa123caa410bf79d36aebff80b04b339b
Tom Hewitt <tom@colourflood.co.uk>
2014-10-31 16:27:25 +0000

    further nav and blur technique testing

--- asset/src/js/bundle.js
+++ asset/src/js/bundle.js
@@ -72,29 +72,8 @@ var Loader = (function () {
 // INIT
 // *****************************************************************
 
-var contentLocation = document.querySelector('section.content');
-GetPage.load('Home', 'pages/home/', contentLocation);
+var $content = document.querySelector('section.content');
+// GetPage.load('Home', 'pages/home/', $content);
 
-
-// CANVAS FOR HERO HOMEPAGE
-var canvas = document.getElementById("heroCanvas");
-var canvasContext = canvas.getContext("2d");
-
-var canvasBackground = new Image();
-canvasBackground.src = "/asset/dist/img/bg.jpg";
-
-var drawBlur = function() {
-	// Store the width and height of the canvas for below
-	var w = canvas.width;
-	var h = canvas.height;
-	
-	// This draws the image we just loaded to our canvas
-	canvasContext.drawImage(canvasBackground, 0, 0, w, h);
-	
-	// This blurs the contents of the entire canvas
-	stackBlurCanvasRGBA("heroCanvas", 0, 0, w, h, 30);
-};
-
-canvasBackground.onload = function() {
-	drawBlur();
-};
+$content.querySelector('.hero').style.height = window.innerHeight + 'px';
+console.log(window.innerHeight);
