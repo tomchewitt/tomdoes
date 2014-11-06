9c3157b7915d23f509cb8e74a247a11e024724b8
Tom Hewitt <tom@colourflood.co.uk>
2014-11-06 01:55:31 +0000

    further styling and font decisions

--- contact/index.php
+++ contact/index.php
@@ -13,29 +13,31 @@ if (isset($_GET["view_as"]) && $_GET["view_as"] == "json") {
 
 		<section class="content contact">
 
-			<h1>
+			<h1 class="main">
 				<span class="word">CONTACT</span>
 				<span class="icon"><svg><use xlink:href="/asset/dist/img/svg-defs.svg#nav-contact"></use></svg></span>
 			</h1>
 
-			<div class="block">
+			<section class="block lead">
 		    	<p>I like hearing from other people. Whether you want to discuss working together, have a job vacancy, have a question, need an answer, want to say hi, or just have a chat - please use any of the below options. I promise I’ll say something nice back.</p>
-			</div>
+			</section>
 
-		    <h2>LET'S CHAT...</h2>
+			<section class="block">
+				<h1>LET'S CHAT</h1>
 
-		    <ul>
-		    	<li>hello (at) tomdoes (dot) digital</li>
-		    	<li>+44 (0) 7947 401 304</li>
-		    	<li>LinkedIN</li>
-		    	<li>Twitter</li>
-		    	<li>Skype</li>
-		    	<li>Behance</li>
-		    	<li>Codepen</li>
-		    	<li>GitHub</li>		    	
-		    </ul>
-
-		    <h2>THANKS &amp; HAVE A NICE DAY!</h2>
+			    <ul class="social">
+			    	<li>hello (at) tomdoes (dot) digital</li>
+			    	<li>+44 (0) 7947 401 304</li>
+			    	<li class="icon linkedin"><a target="_blank" href="">LinkedIN</a></li>
+			    	<li class="icon twitter"><a target="_blank" href="">Twitter</a></li>
+			    	<li class="icon skype"><a target="_blank" href="">Skype</a></li>
+			    	<li class="icon behance"><a target="_blank" href="">Behance</a></li>
+			    	<li class="icon codepen"><a target="_blank" href="">Codepen</a></li>
+			    	<li class="icon github"><a target="_blank" href="">GitHub</a></li>		    	
+			    </ul>
+				
+			    <h2>THANKS &amp; HAVE A NICE DAY!</h2>
+			</section>
 
         </section>
 
