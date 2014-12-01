da285c12f0889f8e3dc0d8bff0520ff46f4bf464
Tom Hewitt <tom@colourflood.co.uk>
2014-12-01 00:02:54 +0000

    final content addition

--- index.php
+++ index.php
@@ -33,42 +33,172 @@ if (isset($_GET["view_as"]) && $_GET["view_as"] == "json") {
 						<ul>
 							<!-- SWAS -->
 							<li id="swas" class="nav-project">
-								<a class="ajax-nav" href="start-with-a-square/" data-pageid="project">
+								<a target="_blank" href="http://startwithasquare.co.uk/my-new-portfolio-goes-live/" data-pageid="project">
 									<img src="/asset/dist/img/work/swas/hero.svg">
 									<span class="gradient"></span>
 									<span class="triangle"></span>
 									<h2>
-										<span class="word">MY PORTFOLIO IS LIVE!</span>
+										<span class="word">MY NEW PORTFOLIO IS LIVE!</span>
 										<span class="subtext">Read on startwithasquare.co.uk</span>
+									</h2>
+								</a>
+							</li>
+
+							<!-- BH: SEC -->
+							<li id="sec" class="nav-project">
+								<a target="_blank" href="https://www.behance.net/gallery/21631129/Square-Enix-Collective" data-pageid="project">
+									<img src="/asset/dist/img/feed/sec.jpg">
+									<span class="gradient"></span>
+									<span class="triangle"></span>
+									<span class="icon"><svg><use xlink:href="#behance"></use></svg></span>
+									<h2>
+										<span class="word">SQUARE ENIX COLLECTIVE</span>
+										<span class="subtext">View on behance.com</span>
+									</h2>
+								</a>
+							</li>
+
+							<!-- CP: DRIPS -->
+							<li id="drips" class="nav-project">
+								<a target="_blank" href="http://codepen.io/tomchewitt/pen/RNWrve" data-pageid="project">
+									<img src="/asset/dist/img/feed/drips.jpg">
+									<span class="gradient"></span>
+									<span class="triangle"></span>
+									<span class="icon"><svg><use xlink:href="#codepen"></use></svg></span>
+									<h2>
+										<span class="word">STRIPEY DRIPS</span>
+										<span class="subtext">Inspect on codepen.io</span>
+									</h2>
+								</a>
+							</li>
+
+							<!-- CP: PYRAMID -->
+							<li id="pyramid" class="nav-project">
+								<a target="_blank" href="http://codepen.io/tomchewitt/pen/vbAGj" data-pageid="project">
+									<img src="/asset/dist/img/feed/pyramid.jpg">
+									<span class="gradient"></span>
+									<span class="triangle"></span>
+									<span class="icon"><svg><use xlink:href="#codepen"></use></svg></span>
+									<h2>
+										<span class="word">3D LOGO PYRAMID</span>
+										<span class="subtext">Inspect on codepen.io</span>
+									</h2>
+								</a>
+							</li>
+
+							<!-- CP: HOVERS -->
+							<li id="hover" class="nav-project">
+								<a target="_blank" href="http://codepen.io/tomchewitt/pen/KAmvo" data-pageid="project">
+									<img src="/asset/dist/img/feed/hover.jpg">
+									<span class="gradient"></span>
+									<span class="triangle"></span>
+									<span class="icon"><svg><use xlink:href="#codepen"></use></svg></span>
+									<h2>
+										<span class="word">SWIRLY HOVER EFFECT</span>
+										<span class="subtext">Inspect on codepen.io</span>
+									</h2>
+								</a>
+							</li>
+
+							<!-- BH: Cosplay -->
+							<li id="cosplay" class="nav-project">
+								<a target="_blank" href="https://www.behance.net/gallery/21709229/Final-Fantasy-XIV-Cosplay-Competition" data-pageid="project">
+									<img src="/asset/dist/img/feed/cosplay.jpg">
+									<span class="gradient"></span>
+									<span class="triangle"></span>
+									<span class="icon"><svg><use xlink:href="#behance"></use></svg></span>
+									<h2>
+										<span class="word">FINAL FANTASY XIV: COSPLAY COMPETITION</span>
+										<span class="subtext">View on behance.com</span>
+									</h2>
+								</a>
+							</li>
+
+
+							<!-- SWAS -->
+							<li id="swas" class="nav-project">
+								<a target="_blank" href="http://startwithasquare.co.uk/do-you-still-need-creative-cloud/" data-pageid="project">
+									<img src="/asset/dist/img/work/swas/hero.svg">
+									<span class="gradient"></span>
+									<span class="triangle"></span>
+									<h2>
+										<span class="word">DO YOU STILL NEED CREATIVE CLOUD?</span>
+										<span class="subtext">Read on startwithasquare.co.uk</span>
+									</h2>
+								</a>
+							</li>
+
+							<!-- BH: Anniversary -->
+							<li id="anniversary" class="nav-project">
+								<a target="_blank" href="https://www.behance.net/gallery/21628275/Anniversary" data-pageid="project">
+									<img src="/asset/dist/img/feed/anniversary.jpg">
+									<span class="gradient"></span>
+									<span class="triangle"></span>
+									<span class="icon"><svg><use xlink:href="#behance"></use></svg></span>
+									<h2>
+										<span class="word">ANNIVERSARY</span>
+										<span class="subtext">View on behance.com</span>
+									</h2>
+								</a>
+							</li>
+
+							<!-- CP: SPINNER -->
+							<li id="spinner" class="nav-project">
+								<a target="_blank" href="http://codepen.io/tomchewitt/pen/WbQwzo" data-pageid="project">
+									<img src="/asset/dist/img/feed/spinner.jpg">
+									<span class="gradient"></span>
+									<span class="triangle"></span>
+									<span class="icon"><svg><use xlink:href="#codepen"></use></svg></span>
+									<h2>
+										<span class="word">PURE CSS ANIMATED SPINNER</span>
+										<span class="subtext">Inspect on codepen.io</span>
 									</h2>
 								</a>
 							</li>
 
 							<!-- SWAS -->
 							<li id="swas" class="nav-project">
-								<a class="ajax-nav" href="start-with-a-square/" data-pageid="project">
+								<a target="_blank" href="http://startwithasquare.co.uk/are-you-a-designer-or-a-developer/" data-pageid="project">
 									<img src="/asset/dist/img/work/swas/hero.svg">
 									<span class="gradient"></span>
 									<span class="triangle"></span>
 									<h2>
-										<span class="word">DO I STILL NEED CREATIVE CLOUD?</span>
+										<span class="word">ARE YOU A DESIGNER OR A DEVELOPER?</span>
 										<span class="subtext">Read on startwithasquare.co.uk</span>
 									</h2>
 								</a>
 							</li>
 
-							<!-- SWAS -->
-							<li id="swas" class="nav-project">
-								<a class="ajax-nav" href="start-with-a-square/" data-pageid="project">
-									<img src="/asset/dist/img/work/swas/hero.svg">
+							<!-- BH: MENSA -->
+							<li id="mensa" class="nav-project">
+								<a target="_blank" href="https://www.behance.net/gallery/21619481/Mensa-Academy" data-pageid="project">
+									<img src="/asset/dist/img/feed/mensa.jpg">
 									<span class="gradient"></span>
 									<span class="triangle"></span>
+									<span class="icon"><svg><use xlink:href="#behance"></use></svg></span>
 									<h2>
-										<span class="word">ARE YOU A DESIGNER OR DEVELOPER?</span>
-										<span class="subtext">Read on startwithasquare.co.uk</span>
+										<span class="word">MENSA ACADEMY</span>
+										<span class="subtext">View on behance.com</span>
 									</h2>
 								</a>
 							</li>
+
+							<!-- CP: FLIPBOOK -->
+							<li id="flipbook" class="nav-project">
+								<a target="_blank" href="http://codepen.io/tomchewitt/pen/gbardp" data-pageid="project">
+									<img src="/asset/dist/img/feed/flipbook.jpg">
+									<span class="gradient"></span>
+									<span class="triangle"></span>
+									<span class="icon"><svg><use xlink:href="#codepen"></use></svg></span>
+									<h2>
+										<span class="word">FLIPBOOK STYLE CAROUSEL</span>
+										<span class="subtext">Inspect on codepen.io</span>
+									</h2>
+								</a>
+							</li>
+
+							
+							
 						</ul>
 					</section>
 			</section>
