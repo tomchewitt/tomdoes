38dee0f491f93f2f7061f846c79dc4acc47230ac
Tom Hewitt <tom@colourflood.co.uk>
2014-12-02 20:38:51 +0000

    further tweaks to the tracking mechanic

--- asset/dist/inc/footer.php
+++ asset/dist/inc/footer.php
@@ -26,15 +26,5 @@
         <!-- <script src="/asset/dist/js/bundle.min.js"></script>  -->
         <script src="/asset/src/js/bundle.js"></script> 
 
-        <!-- ANALYTICS -->
-        <script>
-			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-			ga('create', 'UA-16424514-4', 'auto');
-			ga('send', 'pageview');
-		</script>
-
     </body>
 </html>
--- asset/src/js/module/tracking.js
+++ asset/src/js/module/tracking.js
@@ -1,6 +1,16 @@
 // *****************************************************************
-// PARALLAX ANIMATIONS
+// TRACKING
 // *****************************************************************
+
+// GOOGLE EMBED
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-16424514-4', 'auto');
+ga('send', 'pageview');
+
+// TRACKING MODULE
 var tracking = (function() {
 
 	var tracking = {};
@@ -26,5 +36,5 @@ var tracking = (function() {
 	// RETURN MODULE
 	return tracking;
 
-});
+})();
 
