7579561b0f1d2e776d6b80f3ad80a902651b5624
Tom Hewitt <tom@colourflood.co.uk>
2014-12-02 18:19:35 +0000

    potential fix for overhanging code snippet on lipsum page

--- work/lipsum-generator/index.php
+++ work/lipsum-generator/index.php
@@ -32,7 +32,7 @@ if (isset($_GET["view_as"]) && $_GET["view_as"] == "json") {
 
 				<p>Using vanilla javascript, I wanted to create a good looking, online generator that would allow bookmarking for the user's preferred and most used choice of lorem text. I do this by using html5’s pushstate API to change the query of the URL whenever the user changes the outputted text. This is then bookmark-able so the user can come back to that configuration at the click of a button. The text is preselected by default so just needs the user to copy and paste. There are 4 variables the user can manipulate:</p>
 
-				<p class="lipsum-code"><span>/?paras=1</span>&amp;<span>words=69</span><span>&amp;tags=1</span><span>&amp;hodor=1</span></p>
+				<p class="lipsum-code"><span>/?paras=1</span> <span>&amp;words=69</span> <span>&amp;tags=1</span> <span>&amp;hodor=1</span></p>
 
 				<p>The tags option will add 'p' tags wrapping each paragraph... and the hodor tag is there just for funsies.</p>
 			</section>
