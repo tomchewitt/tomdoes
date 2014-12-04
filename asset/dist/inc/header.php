8f5df90842be892ef97d2c8bd36462aaa6d40844
Tom Hewitt <tom@colourflood.co.uk>
2014-12-04 12:39:34 +0000

    added meta data for better sharing on social sites

--- asset/dist/inc/header.php
+++ asset/dist/inc/header.php
@@ -7,8 +7,16 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title><?php echo $page_title ?> | Tom Does Digital | Online Portfolio of Tom Hewitt | UX. Design. Develop. | @tomchewitt</title>
-        <meta name="description" content="">
+        <meta name="description" content="Tom Does Digital | Online Portfolio of Tom Hewitt | UX. Design. Develop.">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="image_src" content="http://tomdoes.digital/asset/dist/img/tdd-meta.jpg" />
+        <meta name="image_url" content="http://tomdoes.digital/asset/dist/img/tdd-meta.jpg" />
+
+        <meta prefix="og: http://ogp.me/ns#" property="og:title" content="Tom Does Digital | Online Portfolio of Tom Hewitt | UX. Design. Develop." />
+        <meta prefix="og: http://ogp.me/ns#" property="og:type" content="website" />
+        <meta prefix="og: http://ogp.me/ns#" property="og:image" content="http://tomdoes.digital/asset/dist/img/tdd-meta.jpg" />
+        <meta prefix="og: http://ogp.me/ns#" property="og:url" content="http://tomdoes.digital" />
+
         <link rel="icon" type="image/png" href="http://www.tomdoes.digital/asset/dist/img/favicon-32x32.png" sizes="32x32" />
         <link rel="icon" type="image/png" href="http://www.tomdoes.digital/asset/dist/img/favicon-16x16.png" sizes="16x16" />
         <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400italic' rel='stylesheet' type='text/css'>
