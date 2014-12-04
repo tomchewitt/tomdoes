4254de009d14745e49cd1905e6ca0ff8be5ad91a
Tom Hewitt <tom@colourflood.co.uk>
2014-12-04 21:38:25 +0000

    added new images for social sharing

--- asset/dist/inc/header.php
+++ asset/dist/inc/header.php
@@ -11,23 +11,21 @@
         <meta prefix="og: http://ogp.me/ns#" property="og:type" content="website" />
         <meta prefix="og: http://ogp.me/ns#" property="og:title" content="Tom Does Digital | Online Portfolio of Tom Hewitt | UX. Design. Develop." />
         <meta prefix="og: http://ogp.me/ns#" property="og:description" content="Welcome to tomdoes.digital, the new home for my online portfolio. I'm multidisciplinary designer and developer with a passion for great user experiences. Have a look and get in touch." />
+        <meta property="og:image" content="http://www.tomdoes.digital/asset/dist/img/tdd-meta-1200x630.jpg" />
         <meta prefix="og: http://ogp.me/ns#" property="og:url" content="http://tomdoes.digital" />
         <meta prefix="og: http://ogp.me/ns#" property="og:site_name" content="tomdoes.digital" />
-        <meta prefix="og: http://ogp.me/ns#" property="og:image" content="http://www.tomdoes.digital/asset/dist/img/tdd-meta.jpg" />
+        <meta prefix="og: http://ogp.me/ns#" property="og:image" content="http://www.tomdoes.digital/asset/dist/img/tdd-meta-1200x630.jpg" />
         
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="tomdoes.digital">
         <meta name="twitter:domain" content="http://tomdoes.digital">
         <meta name="twitter:title" content="Tom Does Digital | Online Portfolio of Tom Hewitt | UX. Design. Develop." />
         <meta name="twitter:description" content="Welcome to tomdoes.digital, the new home for my online portfolio. I'm multidisciplinary designer and developer with a passion for great user experiences. Have a look and get in touch." />
-        <meta name="twitter:image:src" content="http://www.tomdoes.digital/asset/dist/img/tdd-meta.jpg" />
+        <meta name="twitter:image:src" content="http://www.tomdoes.digital/asset/dist/img/tdd-meta-840x450.jpg" />
 
         <meta itemprop="name" content="Tom Does Digital | Online Portfolio of Tom Hewitt | UX. Design. Develop.">
-        <meta itemprop="image" content="http://www.tomdoes.digital/asset/dist/img/tdd-meta.jpg">
+        <meta itemprop="image" content="http://www.tomdoes.digital/asset/dist/img/tdd-meta-360x297.jpg">
         <meta itemprop="description" content="Welcome to tomdoes.digital, the new home for my online portfolio. I'm multidisciplinary designer and developer with a passion for great user experiences. Have a look and get in touch.">
-
-        <meta name="image_src" content="http://www.tomdoes.digital/asset/dist/img/tdd-meta.jpg" />
-        <meta name="image_url" content="http://www.tomdoes.digital/asset/dist/img/tdd-meta.jpg" />
 
         <link rel="icon" type="image/png" href="http://www.tomdoes.digital/asset/dist/img/favicon-32x32.png" sizes="32x32" />
         <link rel="icon" type="image/png" href="http://www.tomdoes.digital/asset/dist/img/favicon-16x16.png" sizes="16x16" />
@@ -38,6 +36,3 @@
         <?php
             include $_SERVER['DOCUMENT_ROOT']."/asset/dist/img/svg-defs.svg";
         ?>
-
- 
-   
