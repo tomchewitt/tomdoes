441108695480d31f1dc58ebdc0a4fc66d8b87477
Tom Hewitt <tom@colourflood.co.uk>
2015-01-07 14:13:21 +0000

    added final copy for lesdreamers page

--- work/lesdreamers/index.php
+++ work/lesdreamers/index.php
@@ -28,7 +28,11 @@ if (isset($_GET["view_as"]) && $_GET["view_as"] == "json") {
 			</section>
 
 			<section class="block spaced">
-				<p class="headline">...</p>
+				<p class="headline">Lesdreamers is an online style and fashion home created by actress/model Monét Mazur. Called in on the day the site initially went live, I've been helping Monét and her producer get the website up to scratch. Please note, this is a work-in-progress.</p>
+
+				<p>The site needed rebuilding. The previous developers had pushed the site live... broken. The wordpress based backend was almost unusable for Monét and her team, meaning that new content couldn't be added. The site also still needed a lot of work getting it to look like the approved designs.</p>
+
+				<p>After creating a couple of quick fixes, I proposed a quick but radical re-development, building the wordpress theme from scratch, that would allow the content to be controlled by Monét, rather than developers, and would add in some extra functionality in the backend that was missing. I would also tidy up some pages that needed polishing. The site has been relaunched and is gaining some good traction, being picked up by NET-A-PORTER among others. The backend will be evolved and tidied up to better serve the diverse type of content being created, but with a quick turnaround I was able to deliver an MPV that is functional and successful.</p>
 			</section>
 			<section class="block meta">
 				<ul class="triple">
